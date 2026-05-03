0300bb7aa3344d96c2ab1fc38d421f6efe6217be
Vladikbadik <vlad.badion@gmail.com>
2026-05-04 00:23:32 +0300

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        char[] s_array = s.toCharArray();
+        char[] t_array = t.toCharArray();
+
+        Arrays.sort(s_array);
+        Arrays.sort(t_array);
+
+        boolean equal = Arrays.equals(s_array, t_array);
+
+        return equal;
+
+    }
+}
